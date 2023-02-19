7767a59559c11abd3ed3e5d0e6f673d33f6434d7
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-02-19 18:39:59 -0300

    feat:adding checkbox event value reactive

--- interface.py
+++ interface.py
@@ -12,7 +12,42 @@ form_label_tolerated_error = tkinter.Label(window, text="Digite o Erro Tolerado:
 input_taxa = tkinter.Entry(window)
 input_tolerated_error = tkinter.Entry(window)
 
-submit_button = tkinter.Button(window, text="Realizar Aprendizado")
+
+def submit_button_event():
+    taxa = input_taxa.get()
+    tolerated_error = input_tolerated_error.get()
+    print(taxa)
+    print(tolerated_error)
+    # logica do submit
+
+
+def test_button_event():
+    for i, row in enumerate(checkboxes):
+        for j, var in enumerate(row):
+            if var.get() == 1:
+                print("CheckBox %d,%d está selecionado" % (i, j))
+                # LOGICA DO SELECIONADO
+
+def on_select(value):
+    print("Selecionado:", value)
+
+
+submit_button = tkinter.Button(window, text="Realizar Aprendizado", command=submit_button_event)
+
+
+
+
+options = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3', 'D1', 'D2', 'D3', 'E1', 'E2', 'E3', 'J1', 'J2', 'J3', 'K1', 'K2', 'K3']
+
+# Crie o menu suspenso
+variable = tkinter.StringVar()
+
+option_menu = tkinter.OptionMenu(window, variable, *options)
+
+variable.trace("w", lambda name, index, mode, variable=variable: on_select(variable.get()))
+
+option_menu.place(x=250, y=200)
+
 
 form_label_taxa.place(x=100, y=50)
 input_taxa.place(x=350, y=50)
@@ -29,22 +64,6 @@ for i in range(9):
         checkbox.place(x=320 + j * 30, y=200 + i * 25)
         row.append(var)
     checkboxes.append(row)
-
-def submit_button_event():
-    taxa=input_taxa.get()
-    tolerated_error=input_tolerated_error.get()
-    print(taxa)
-    print(tolerated_error)
-    #logica do submit
-
-
-def test_button_event():
-    for i, row in enumerate(checkboxes):
-        for j, var in enumerate(row):
-            if var.get() == 1:
-                print("CheckBox %d,%d está selecionado" % (i, j))
-                #LOGICA DO SELECIONADO
-
 
 button_test = tkinter.Button(window, text="Realizar Teste", command=test_button_event)
 button_test.place(x=5 + j * 60, y=200 + i * 30)
