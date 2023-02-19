6f41e8a943ecff550ece55ed363c4eb1c044d406
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-02-19 16:10:29 -0300

    feat:adding dialog

---
+++ interface.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+window = Tk()
+window.title("Welcome to TutorialsPoint")
+window.geometry('1920x1080')
+window.configure(background = "grey");
+
+window.mainloop()
